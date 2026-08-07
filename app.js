6015e4aafef9ea1fb4f00327ba07045d2a2d9f80
ChiliCode <tilinrodrigo3@gmail.com>
2026-08-07 12:52:05 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1246,6 +1246,28 @@ function createActiveEquipmentChecklist(bio) {
           </button>
         </div>
       `}
+
+      <!-- Hamster Wheel Animation for Active Equipment Card (Uiverse.io by Nawsome) -->
+      <div class="hamster-anim-wrapper">
+        <div aria-label="Orange and tan hamster running in a metal wheel" role="img" class="wheel-and-hamster">
+          <div class="wheel"></div>
+          <div class="hamster">
+            <div class="hamster__body">
+              <div class="hamster__head">
+                <div class="hamster__ear"></div>
+                <div class="hamster__eye"></div>
+                <div class="hamster__nose"></div>
+              </div>
+              <div class="hamster__limb hamster__limb--fr"></div>
+              <div class="hamster__limb hamster__limb--fl"></div>
+              <div class="hamster__limb hamster__limb--br"></div>
+              <div class="hamster__limb hamster__limb--bl"></div>
+              <div class="hamster__tail"></div>
+            </div>
+          </div>
+          <div class="spoke"></div>
+        </div>
+      </div>
     </div>
   `;
   
